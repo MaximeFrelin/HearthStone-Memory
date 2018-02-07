3b2235b742b5abe51d269637a0dc113312ceb587
Frelin Maxime <frelinmaxime@gmail.com>
2018-02-07 09:28:01 +0100

    Ajout des cartes retourner et qui se retourne et de la comparaison

---
+++ jeu.js
@@ -0,0 +1,66 @@
+this.duoCarte = [];
+
+function afficherCarte(id) {
+    var indiceCarte = trouverIndiceCarte(id);
+    var src = constructionSrc(indiceCarte);
+    var carteARetourner = document.getElementById(id).src = src;
+    comparerCarte(id);
+}
+
+function cacherCarte(id) {
+    var carteARetourner = document.getElementById(id).src = "src/carte/dos.png";
+}
+
+function trouverIndiceCarte(nomComplet) {
+    var indiceCarte = nomComplet.split("/");
+    return indiceCarte[1];
+}
+
+function constructionSrc(indiceCarte) {
+    var src = "src/carte/carte";
+    if(indiceCarte == 1) 
+        return src+="1.png";
+    else if(indiceCarte == 2)
+        return src+="2.png";
+    else if(indiceCarte == 3)
+        return src+="3.png";
+    else if(indiceCarte == 4)
+        return src+="4.png";
+    else if(indiceCarte == 5)
+        return src+="5.png";
+    else if(indiceCarte == 6)
+        return src+="6.png";
+    else if(indiceCarte == 7)
+        return src+="7.png";
+    else if(indiceCarte == 8)
+        return src+="8.png";
+}
+
+function comparerCarte(indiceCarte) {
+    if(duoCarte[0] == null)
+        duoCarte[0] = indiceCarte;
+    else if(duoCarte[1] == null)
+        duoCarte[1] = indiceCarte;
+    if(duoCarte[0] != null && duoCarte[1] != null) {
+        console.log(duoCarte[0] != duoCarte[1])
+        if(duoCarte[0] != duoCarte[1]) {
+            var indiceCarte1 = trouverIndiceCarte(duoCarte[0]);
+            var indiceCarte2 = trouverIndiceCarte(duoCarte[1]);
+            if (indiceCarte1 == indiceCarte2) {
+                console.log("Ta gagné");
+            }else {
+                setTimeout(function(){cacherCarte(duoCarte[0]); cacherCarte(duoCarte[1]);}, 500);
+            }
+                
+        }
+        duoCarte[0] = null;
+        duoCarte[1] = null;
+    }
+}
+
+function attendre(milisecondes) {
+    var currentTime = new Date().getTime();
+ 
+    while (currentTime + milisecondes >= new Date().getTime()) {
+    }
+ }
--- plateau.js
+++ plateau.js
@@ -1,6 +1,6 @@
 var plateau = document.getElementById("plateau_carte");
 var taillePlateau = 4;
-var tableauCarte = new Array("carte1", "carte2", "carte3", "carte4", "carte5", "carte6", "carte7", "carte8","carte1", "carte2", "carte3", "carte4", "carte5", "carte6", "carte7", "carte8");
+var tableauCarte = new Array("carte1/1", "carte2/2", "carte3/3", "carte4/4", "carte5/5", "carte6/6", "carte7/7", "carte8/8","carte9/1", "carte10/2", "carte11/3", "carte12/4", "carte13/5", "carte14/6", "carte15/7", "carte16/8");
 
 /**
  * Fonction pour construire le plateau de jeu
@@ -8,15 +8,18 @@ var tableauCarte = new Array("carte1", "carte2", "carte3", "carte4", "carte5", "
 function construirePlateau() {
     this.carteTotal = "";
     melange(tableauCarte);
-    console.log(tableauCarte);
     for (i = 0; i < Math.pow(taillePlateau,2); i++) {
         if (i%taillePlateau == 0)
             carteTotal += " </br>";
-        carteTotal += "<input type='image' src='src/carte/" + tableauCarte[i] + ".png' height='200px' weight='100' id='carte" + i + "'>";             
+        carteTotal += "<input onclick='afficherCarte(this.id)' type='image' src='src/carte/dos.png' height='200px' weight='100' id=" + tableauCarte[i] + ">";             
     }
     plateau.innerHTML = carteTotal;
 }
 
+/**
+ * Fonction pour mélanger le plateau de carte
+ * @param {*} carte 
+ */
 function melange(carte){
     for (var i=0; i<carte.length; i++){
         var alea=Math.floor(Math.random()*carte.length);
